70e79dfd070a95cabf93bad0f824793249b8109d
Daiki Itoh <dijp9707@gmail.com>
2020-03-14 23:57:11 -0500

    Customize admin for course and assignment 11:57 3/14

--- draw_hw/courses/admin.py
+++ draw_hw/courses/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['code', 'class_name', 'instructor']
 
-admin.site.register(models.Assignment)
+    search_fields = ['class_name']
+
+    list_filter = []
+
+class AssignmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'course', 'deadline', 'pdf']
+
+    search_fields = ['course']
+
+    list_filter = []
+
+admin.site.register(models.Course, CourseAdmin)
+
+admin.site.register(models.Assignment, AssignmentAdmin)
